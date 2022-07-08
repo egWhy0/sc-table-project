0f1de551ce048d40a11340495b4ccc3a494591f6
egWhy0@gmail.com <egWhy0@gmail.com>
2022-07-08 15:11:46 +0900

    sc-table-project first commit

---
+++ Backend/src/main/java/com/sc/sctable/ScTableApplication.java
@@ -0,0 +1,13 @@
+package com.sc.sctable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScTableApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/sc/sctable/ScTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.sctable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
